9b1f52af1066e8129e3a4b3223eac141cf374df0
Chaz <charlesbarker@lbcstudios.ca>
2021-05-01 11:31:45 -0700

    convert dummy data into Firebase style data

--- src/hooks/useDummyData.js
+++ src/hooks/useDummyData.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 // fake data generator
 const getItems = (count, offset = 0) => {
@@ -8,10 +9,41 @@ const getItems = (count, offset = 0) => {
 	}));
 };
 
+const Initial_Data = [
+	{ uid: 0, movieTitle: 50 },
+	{ uid: 1, movieTitle: "swimming" },
+	{ uid: 2, movieTitle: "drunking" },
+	{ uid: 3, movieTitle: "pooping" },
+];
+
+const GetInitialData = () => {
+	var newArray = [];
+
+	// Since the array is initialized with `undefined` on each position,
+	// the value of `v` below will be `undefined`
+	//'v' is the current value at that position in the array
+	//'k' is the index of the position in the array
+	newArray = Array.from(
+		/*an object with a "length" property*/ { length: Initial_Data.length },
+		(v, k) => k
+	).map((k) => ({
+		//uid:must be a string because it is used as a draggableId for react-beautiful-dnd
+		//uid:appending a uuidv4 to guarantee uniqueness
+
+		uid: `item-${Initial_Data[k].uid}-${uuidv4()}`,
+		movieTitle: Initial_Data[k].movieTitle,
+	}));
+
+	//newArray = Array.from(Initial_Data);
+	return newArray;
+};
+
 const useDummyData = (userId, boardId) => {
 	const [final, setFinal] = useState(null);
 
-	const [state, setState] = useState([getItems(10), getItems(5, 10)]);
+	//const [state] = useState([getItems(10), getItems(5, 10)]);
+
+	const [state] = useState([GetInitialData(), GetInitialData()]);
 
 	useEffect(() => {
 		const finalTasks = [];
