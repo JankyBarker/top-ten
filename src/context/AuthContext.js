1cea86ce77ec73c81d7d5238eeb26c999372c4e3
Chaz <charlesbarker@lbcstudios.ca>
2021-05-24 10:05:24 -0700

    Fixed incorrect naming for Context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,20 +1,20 @@
 import * as React from "react";
 import useFirebaseAuth from "../hooks/useFirebaseAuth";
-const CountContext = React.createContext();
+const AuthContext = React.createContext();
 //https://kentcdodds.com/blog/how-to-use-react-context-effectively
 //try using Reducer here? - see above
 
 function AuthProvider({ children }) {
 	const AuthAPI = useFirebaseAuth();
 	return (
-		<CountContext.Provider value={AuthAPI}>{children}</CountContext.Provider>
+		<AuthContext.Provider value={AuthAPI}>{children}</AuthContext.Provider>
 	);
 }
 
 function useAuth() {
-	const context = React.useContext(CountContext);
+	const context = React.useContext(AuthContext);
 	if (context === undefined) {
-		throw new Error("useCount must be used within a <CountProvider>");
+		throw new Error("useCount must be used within a <AuthContext.Provider>");
 	}
 	return context;
 }
